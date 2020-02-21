dc1e96f914ecf5711e152adafa7a514db240cf04
nebosite <ericjorg@thejcrew.net>
2020-02-21 08:34:54 -0800

    Initial Parent/Child parsing with tests

---
+++ AlfredTests/TestDocumentSection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Alfred;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlfredTests
+{
+    [TestClass]
+    public class TestDocumentSection
+    {
+        [TestMethod]
+        public void ParseCanHandleSingleLineItem()
+        {
+            string text = @"This is an item  ";
+
+            var results = DocumentSection.Parse(text);
+            Assert.AreEqual(1, results.Length);
+            Assert.AreEqual(0, results[0].IndentSize);
+            Assert.AreEqual("This is an item", results[0].Contents);
+        }
+
+        [TestMethod]
+        public void ParseCanHandleMultipleItems()
+        {
+            string text = @"    hey 
+
+            
+    hey2     
+       child1
+
+       child2
+        
+  Last hey   ";
+
+            var results = DocumentSection.Parse(text);
+            //Assert.AreEqual(6, results.Length);
+
+            var itemIndex = 0;
+            var item = results[itemIndex++];
+            Assert.AreEqual(0, item.Children.Count);
+            Assert.AreEqual(4, item.IndentSize);
+            Assert.AreEqual("hey", item.Contents);
+
+            item = results[itemIndex++];
+            Assert.AreEqual(0, item.Children.Count);
+            Assert.AreEqual(0, item.IndentSize);
+            Assert.AreEqual("", item.Contents);
+
+            item = results[itemIndex++];
+            Assert.AreEqual(0, item.Children.Count);
+            Assert.AreEqual(0, item.IndentSize);
+            Assert.AreEqual("", item.Contents);
+
+            item = results[itemIndex++];
+            Assert.AreEqual(3, item.Children.Count);
+            Assert.AreEqual(4, item.IndentSize);
+            Assert.AreEqual("hey2", item.Contents);
+            foreach(var child in item.Children)
+            {
+                Assert.AreEqual(0, child.Children.Count);
+                Assert.AreEqual(7, child.IndentSize);
+            }
+            Assert.AreEqual("child1", item.Children[0].Contents);
+            Assert.AreEqual("", item.Children[1].Contents);
+            Assert.AreEqual("child2", item.Children[2].Contents);
+
+            item = results[itemIndex++];
+            Assert.AreEqual(0, item.Children.Count);
+            Assert.AreEqual(0, item.IndentSize);
+            Assert.AreEqual("", item.Contents);
+
+            item = results[itemIndex++];
+            Assert.AreEqual(0, item.Children.Count);
+            Assert.AreEqual(2, item.IndentSize);
+            Assert.AreEqual("Last hey", item.Contents);
+
+        }
+
+
+    }
+}
---
+++ DocumentSection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Alfred
+{
+    public class DocumentSection
+    {
+        public int IndentSize { get; set; }
+        public string Contents { get; set; }
+        public List<DocumentSection> Children { get; set; } = new List<DocumentSection>();
+
+        public static DocumentSection[] Parse(string text)
+        {
+            var output = new List<DocumentSection>();
+            var blankLineEscrow = new List<DocumentSection>();
+            var lines = text.Split('\n');
+            for(int i = 0; i < lines.Length; i++)
+            {
+                var currentLine = lines[i].Trim();
+                var indentSize = 0;
+                var spaceMatch = Regex.Match(lines[i], @"^(\s*)[^\s]");
+                if (spaceMatch.Success) indentSize = spaceMatch.Groups[1].Value.Length;
+                var newSection = new DocumentSection()
+                    { 
+                        Contents = currentLine,
+                        IndentSize = indentSize
+                    };
+
+                if(newSection.Contents == "")
+                {
+                    blankLineEscrow.Add(newSection);
+                }
+                else
+                {
+                    // Look for a parent
+                    var parentFound = false;
+                    for(int j = output.Count-1; j >=0; j--)
+                    {
+                        if(output[j].Contents != "" && newSection.IndentSize > output[j].IndentSize)
+                        {
+                            foreach(var blank in blankLineEscrow)
+                            {
+                                blank.IndentSize = newSection.IndentSize;
+                                output[j].Children.Add(blank);
+                            }
+                            blankLineEscrow.Clear();
+                            output[j].Children.Add(newSection);
+                            parentFound = true;
+                            break;
+                        }
+                    }
+                    if (!parentFound)
+                    {
+                        output.AddRange(blankLineEscrow);
+                        blankLineEscrow.Clear();
+                        output.Add(newSection);
+                    }
+                }
+
+            }
+            return output.ToArray();
+        }
+    }
+}
